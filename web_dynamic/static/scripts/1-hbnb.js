872587f77c99e465e8bb206e7e416bc0ea7baee6
Aleia DeVore <2496@holbertonschool.com>
2021-05-16 18:24:54 +0000

    semistandard fixes

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,17 @@
-/* jQuery script that listens for changes on each INPUT checkbox tag*/
+/* jQuery script that listens for changes on each INPUT checkbox tag */
 /* global $ */
 $('document').ready(function () {
-  selected = []
-  $('li input').click( function () {
-    if ($(this).prop('checked') == true) {
+  const selected = [];
+  $('li input').click(function () {
+    if ($(this).prop('checked') === true) {
       selected.push($(this).attr('data-name'));
     } else {
       delete selected.pop($(this).attr('data-name'));
     }
-    if (selected.length == 0) {
-        $('#selected').html('&nbsp;')
-    }
-    else {
-    $('#selected').text(selected.join(', '));
+    if (selected.length === 0) {
+      $('#selected').html('&nbsp;');
+    } else {
+      $('#selected').text(selected.join(', '));
     }
   });
 });
